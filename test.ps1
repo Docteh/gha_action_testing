6336c3a761431c8618f3ff6fd850301df14a582f
Kyle Kienapfel <Docteh@users.noreply.github.com>
2022-07-17 06:29:38 -0700

    more refinement

--- test.ps1
+++ test.ps1
@@ -3,12 +3,22 @@ param($BUILD_NAME)
 $GITDATE = $(git show -s --date=short --format='%ad') -replace "-", ""
 $GITREV = $(git show -s --format='%h')
 
-# Write out a BUILD_TAG for the later steps (Upload)
-echo "BUILD_TAG=$GITREV" >> $env:GITHUB_ENV
-echo "wrote out $GITREV to the env file"
+# Write out a tag for later steps (Probably Upload)
+# We're getting ${{ github.event.number }} as $env:PR_NUMBER"
+echo "PR_NUMBER: $env:PR_NUMBER"
+$PR_NUMBER = $env:PR_NUMBER.Substring(2) -as [int]
+$PR_NUMBER_TAG = "pr"+([string]$PR_NUMBER).PadLeft(5,'0')
+if ("$env:PR_NUMBER" -eq "pr"){
+    $env:PR_NUMBER = "manual"
+    echo "BUILD_TAG=manual-$GITDATE-$GITREV" >> $env:GITHUB_ENV
+} else {
+    echo "BUILD_TAG=verify-$PR_NUMBER_TAG-$GITDATE-$GITREV" >> $env:GITHUB_ENV
+}
+echo "PR_NUMBER: $env:PR_NUMBER"
 
-echo lol
-# ${{ github.event.number }}
+
+
+
 # yuzu-msvc-verify-pr9203-date-date
 dir
 mkdir artifacts
