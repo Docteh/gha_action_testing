0a4bb5a2bafdc04f33472968b61cebf832400582
Kyle Kienapfel <Docteh@users.noreply.github.com>
2022-07-17 17:51:28 -0700

    looking at more variables from gha

--- test.ps1
+++ test.ps1
@@ -3,6 +3,17 @@ param($BUILD_NAME)
 $GITDATE = $(git show -s --date=short --format='%ad') -replace "-", ""
 $GITREV = $(git show -s --format='%h')
 
+$GITSHA = $(git show -s --format='%H')
+if ("$GITSHA" -ne $env:GITHUB_SHA) {
+    echo "two hashes don't match, come back to this section later"
+} else {
+    echo "two hashes do match"
+}
+echo "$env:GITHUB_SHA $GITREV"
+
+
+
+
 # Write out a tag for later steps (Probably Upload)
 # We're getting ${{ github.event.number }} as $env:PR_NUMBER"
 $PR_NUMBER = $env:PR_NUMBER.Substring(2) -as [int]
@@ -16,7 +27,20 @@ if ("$env:PR_NUMBER" -eq "pr"){
 echo "PR_NUMBER: $env:PR_NUMBER"
 
 if ("$env:GITHUB_ACTIONS" -eq "true") {
-    echo "yolo?"
+    echo "printing out a good chunk of env for testing purposes"
+    echo "GITHUB_ACTION: $env:GITHUB_ACTION"
+    echo "GITHUB_ACTION_REPOSITORY: $env:GITHUB_ACTION_REPOSITORY"
+    echo "GITHUB_ACTOR: $env:GITHUB_ACTOR"
+    echo "GITHUB_BASE_REF: $env:GITHUB_BASE_REF"
+    echo "GITHUB_HEAD_REF: $env:GITHUB_HEAD_REF"
+    echo "GITHUB_REF: $env:GITHUB_REF"
+    echo "GITHUB_REF_NAME: $env:GITHUB_REF_NAME"
+    echo "GITHUB_REF_TYPE: $env:GITHUB_REF_TYPE"
+    echo "GITHUB_SHA: $env:GITHUB_SHA"
+    echo "GITHUB_WORKFLOW: $env:GITHUB_WORKFLOW"
+    echo "RUNNER_ARCH: $env:RUNNER_ARCH"
+    echo "RUNNER_NAME: $env:RUNNER_NAME"
+    echo "RUNNER_OS: $env:RUNNER_OS"
 }
 
 
