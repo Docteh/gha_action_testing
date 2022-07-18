a0efa0d762da541ca44a8606920662d6f3262c42
Kyle Kienapfel <Docteh@users.noreply.github.com>
2022-07-17 19:24:26 -0700

    alternative logic

--- test.ps1
+++ test.ps1
@@ -24,9 +24,15 @@ echo "$env:GITHUB_SHA $GITREV"
 # Invalid workflow file: .github/workflows/pr-verify.yml#L9
 # The workflow is not valid. .github/workflows/pr-verify.yml (Line: 9, Col: 12): A mapping was not expected
 
-echo "Showing last 3 commits`n"
+echo "`nShowing last 3 commits"
 git log --oneline -n 3
 
+if ($env:GITHUB_EVENT_NAME -eq "pull_request") {
+    $GITREV_MERGE = $(git rev-parse HEAD)
+    $GITREV_PULL = $(git rev-parse HEAD~1)
+    $GITREV_MAIN = $(git rev-parse HEAD~2)
+    $GITREV = $GITREV_PULL.Substring(9)
+}
 
 # Write out a tag for later steps (Probably Upload)
 # We're getting ${{ github.event.number }} as $env:PR_NUMBER"
@@ -41,11 +47,12 @@ if ("$env:PR_NUMBER" -eq "pr"){
 echo "PR_NUMBER: $env:PR_NUMBER"
 
 if ("$env:GITHUB_ACTIONS" -eq "true") {
-    echo "printing out a good chunk of env for testing purposes"
+    echo "`nprinting out a good chunk of env for testing purposes"
     echo "GITHUB_ACTION: $env:GITHUB_ACTION"
     echo "GITHUB_ACTION_REPOSITORY: $env:GITHUB_ACTION_REPOSITORY"
     echo "GITHUB_ACTOR: $env:GITHUB_ACTOR"
     echo "GITHUB_BASE_REF: $env:GITHUB_BASE_REF"
+    echo "GITHUB_EVENT_NAME: $env:GITHUB_EVENT_NAME"
     echo "GITHUB_HEAD_REF: $env:GITHUB_HEAD_REF"
     echo "GITHUB_REF: $env:GITHUB_REF"
     echo "GITHUB_REF_NAME: $env:GITHUB_REF_NAME"
